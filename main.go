616a9d810d78f407bfd9e94d034cf310a8f8bf58
Shimon K. Cohen <shimoco@gmail.com>
2023-04-14 20:15:01 +0300

    put and delete

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
+	"os"
 	"qziz/handlers"
 	"qziz/models"
 )
@@ -11,6 +15,15 @@ var questionsHandler *handlers.QuestionsHandler
 
 func init() {
 	questions = make([]models.Question, 0)
+	file, err := os.ReadFile("questions.json")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	err = json.Unmarshal([]byte(file), &questions)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println(len(questions))
 	questionsHandler = handlers.NewQuestionHandler(questions)
 }
 
@@ -18,5 +31,7 @@ func main() {
 	router := gin.Default()
 	router.POST("/questions", questionsHandler.AddNewQuestion)
 	router.GET("/questions", questionsHandler.ListQuestions)
+	router.PUT("/questions/:id", questionsHandler.UpdateQuestion)
+	router.DELETE("/questions/:id", questionsHandler.DeleteQuestion)
 	router.Run()
 }
