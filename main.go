71bdb6d18196b134d87d78c3c32b199995182821
Shimon K. Cohen <shimoco@gmail.com>
2023-04-14 12:40:18 +0300

    get and post

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"qziz/handlers"
+	"qziz/models"
+)
+
+var questions []models.Question
+var questionsHandler *handlers.QuestionsHandler
+
+func init() {
+	questions = make([]models.Question, 0)
+	questionsHandler = handlers.NewQuestionHandler(questions)
+}
 
 func main() {
 	router := gin.Default()
+	router.POST("/questions", questionsHandler.AddNewQuestion)
+	router.GET("/questions", questionsHandler.ListQuestions)
 	router.Run()
 }
